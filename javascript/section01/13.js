1b5500660db43747752375b8ee42f5b85bf764c8
riskerGoldClass <roakfwk30@naver.com>
2022-08-03 23:34:34 +0900

    some modified2

--- javascript/section01/13.js
+++ javascript/section01/13.js
@@ -3,11 +3,14 @@ function solution(str){
     let answer = "";
     for(i = 0; i < str.length; i++){
 
-        if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-            answer += String.fromCharCode(str.charCodeAt(i)+32);
-        }else{
-            answer += String.fromCharCode(str.charCodeAt(i)-32);
-        }
+        answer += (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) ? String.fromCharCode(str.charCodeAt(i)+32) : String.fromCharCode(str.charCodeAt(i)-32);
+        
+
+        // if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+        //     answer += String.fromCharCode(str.charCodeAt(i)+32);
+        // }else{
+        //     answer += String.fromCharCode(str.charCodeAt(i)-32);
+        // }
 
     }
     return answer;
--- javascript/section01/14.js
+++ javascript/section01/14.js
@@ -14,9 +14,13 @@ function solution(arr){
         if(max < j) max = j;
     }
 
-    let index = eachCount.indexOf(max);
-    return arr[index];
+    return arr[eachCount.indexOf(max)];
 
 }
 
+
+const start = new Date();
 console.log(solution(['teacher', 'time', 'student', 'beautiful', 'good']));
+const end = new Date();
+
+console.log("Running Time : " + (end - start)/100 + " sec");
