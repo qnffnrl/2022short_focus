029ae0d43c695073357d877a6e168577c3cd9cf9
risker <roakfwk30@naver.com>
2022-08-08 17:32:49 +0900

    day10 end / react start

---
+++ react/client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import MyTag from './MyTag'
+
+function App() {
+  return (
+    <div id='page'>
+      <div>
+        <h1>Hello React</h1>
+        <h3>risker</h3>
+      </div>
+      <MyTag></MyTag>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/client/src/MyTag.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MyTag extends Component {
+
+    render(){
+        return (
+            <h3><em>User Define Component</em></h3>
+        )
+    }
+
+}
+
+export default MyTag;
