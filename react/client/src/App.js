8b2ec3ae8e2942a49f5829987e991fd083ae1f13
519강의실 <roakfwk30@naver.com>
2022-08-10 16:43:47 +0900

    day12 end

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import MyTag from './MyTag'
-import MyTag2 from './MyTag2';
-import MyTag3 from './MyTag3';
-import LC from './LifeCycle';
-import Class from './Class';
+// import MyTag from './MyTag'
+// import MyTag2 from './MyTag2';
+// import MyTag3 from './MyTag3';
+// import LC from './LifeCycle';
+// import Class from './Class';
+// import ES6 from './es6/es6'
+import List from './tour/list';
+import Footer from './tour/footer'
 
 function App() {
   return (
     <div id='page'>
-        <h1>Hello React</h1>
-        <h3>risker</h3>
-        <MyTag></MyTag>
-        <MyTag2></MyTag2>
-        <MyTag3></MyTag3>
-        <LC userName="Hong"></LC>
-        <Class></Class>
+        <h1>GwangJu City Tour List</h1>
+        <div id='listSector'>
+          <List></List>
+          <Footer></Footer>
+        </div>
     </div>
   );
 }
